97dd1a4d8113324e7467b2035cc360638e360b7d
Luiz Hélio <luizhelionerd@hotmail.com>
2024-06-05 13:47:21 -0300

    Update Cadastro.py

--- Project/Cadastro.py
+++ Project/Cadastro.py
@@ -1,15 +1,15 @@
 # Criação da matriz
 matriz_vagas = []
 
-vagas_coluna = ['Dev. Java', 'Analista de dados', 'Ux Designer', 'Dev. FrontEnd', 'Designer Gráfico']
-skills_linha = ['Java', 'Python', 'SQL', 'Git', 'Spring', 'Figma', 'Comunicação', 'Power BI']
+vagas_linha = ['Dev. Java', 'Analista de dados', 'Ux Designer', 'Dev. FrontEnd', 'Designer Gráfico']
+skills_coluna = ['Java', 'Python', 'SQL', 'Git', 'Spring', 'Figma', 'Comunicação', 'Power BI']
 
 # Cadastrar dados na matriz
 for i in range(5):
     vagas = []
     for j in range(8):
         while True:
-            resposta = input(f"A vaga '{vagas_coluna[i]}' utiliza a skill '{skills_linha[j]}'? (S/N): ")
+            resposta = input(f"A vaga '{vagas_linha[i]}' utiliza a skill '{skills_coluna[j]}'? (S/N): ")
             if resposta.upper() == "S":
                 vagas.append(True)
                 break
@@ -21,7 +21,13 @@ for i in range(5):
     matriz_vagas.append(vagas)
 
 # Mostrar a matriz (teste)
-for vagas in matriz_vagas:
-    for elemento in vagas:
-        print(elemento, end=' ')
+print(" ", end=' ')
+for skill in skills_coluna:
+    print("{:<20}".format(skill), start='                  ', end=' ')
+print()
+
+for i in range(len(matriz_vagas)):
+    print("{:<20}".format(vagas_linha[i]), end=' ')
+    for elemento in matriz_vagas[i]:
+        print("{:<20}".format(str(elemento)), end=' ')
     print()
