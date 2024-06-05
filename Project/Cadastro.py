c2812074f03c4d83fa2e1771dcace36b3cdaceac
Luiz Hélio <luizhelionerd@hotmail.com>
2024-06-05 18:31:55 -0300

    Update Cadastro.py
    Mudança das skills para linha e vagas para coluna

--- Project/Cadastro.py
+++ Project/Cadastro.py
@@ -3,16 +3,16 @@ import os
 # Criação da matriz
 matriz_vagas = []
 
-vagas_linha = ['Dev. Java', 'Analista de dados', 'Ux Designer', 'Dev. FrontEnd', 'Designer Gráfico']
-skills_coluna = ['Java', 'Python', 'SQL', 'Figma', 'Comunicação', 'Power BI']
+skills_linha = ['Java', 'Python', 'SQL', 'Figma', 'Comunicação', 'Power BI']
+vagas_coluna = ['Dev. Java', 'Analista de dados', 'Ux Designer', 'Dev. FrontEnd', 'Designer Gráfico']
 
 while True:
     print("------------------------------")
     print("        MENU PRINCIPAL        ")
     print("------------------------------")
     print("1 - Cadastrar Dados na Planilha")
-    print("2 - Cadastrar Uma nova vaga")
-    print("3 - Cadastrar Uma nova Skill")
+    print("2 - Cadastrar Uma nova Skill")
+    print("3 - Cadastrar Uma nova Vaga")
     print("4 - Alterar Dados na Planilha")
     print("5 - Ver Planilha")
     print("0 - Sair")
@@ -21,46 +21,28 @@ while True:
     match resposta:
         case "1": 
         # Cadastrar dados na matriz
-            for i in range(len(vagas_linha)):
-                vagas = []
-                for j in range(len(skills_coluna)):
+            for i in range(len(skills_linha)):
+                skills = []
+                for j in range(len(vagas_coluna)):
                     while True:
-                        resposta = input(f"A vaga '{vagas_linha[i]}' utiliza a skill '{skills_coluna[j]}'? (S/N): ")
+                        resposta = input(f"A vaga '{vagas_coluna[j]}' utiliza a skill '{skills_linha[i]}'? (S/N): ")
                         if resposta.upper() == "S":
-                            vagas.append(True)
+                            skills.append(True)
                             break
                         elif resposta.upper() == "N":
-                            vagas.append(False)
+                            skills.append(False)
                             break
                         else:
                             print("RESPOSTA INVALIDA, Digite S ou N")
-                matriz_vagas.append(vagas)
+                matriz_vagas.append(skills)
 
         case "2":
-        # Cadastrar uma nova vaga
-            while True:
-                nova_Vaga = input("Qual vaga deseja adicionar?\n> ")
-                conferir = input(f"{nova_Vaga} está correto? (S/N)\n>")
-                if conferir.upper() == "S":
-                    vagas_linha.append(nova_Vaga)
-
-                    if input("Deseja Adicionar outra vaga? (S/N)\n> ").upper() == "S":
-                        continue
-                    else:
-                        break
-                elif conferir.upper() == "N":
-                    continue
-                else:
-                    print("ERRO: RESPOSTA INVALIDA")
-                    continue
-            
-        case "3":
-        # Cadastrar uma nova Skill
+        # Cadastrar uma nova skill
             while True:
                 nova_Skill = input("Qual skill deseja adicionar?\n> ")
                 conferir = input(f"{nova_Skill} está correto? (S/N)\n>")
                 if conferir.upper() == "S":
-                    skills_coluna.append(nova_Skill)
+                    skills_linha.append(nova_Skill)
 
                     if input("Deseja Adicionar outra skill? (S/N)\n> ").upper() == "S":
                         continue
@@ -71,56 +53,73 @@ while True:
                 else:
                     print("ERRO: RESPOSTA INVALIDA")
                     continue
+            
+        case "3":
+        # Cadastrar uma nova vaga
+            while True:
+                nova_vaga = input("Qual vaga deseja adicionar?\n> ")
+                conferir = input(f"{nova_vaga} está correto? (S/N)\n>")
+                if conferir.upper() == "S":
+                    vagas_coluna.append(nova_vaga)
+
+                    if input("Deseja Adicionar outra vaga? (S/N)\n> ").upper() == "S":
+                        continue
+                    else:
+                        break
+
         case "4":
         # Alterar dados na matriz
+            for skills in range(len(skills_linha)):
+                print(f"{skills+1} - {skills_linha[skills]}")
+            skill_alterar = int(input("Qual skill deseja alterar?\n> "))
+
+            for vagas in range(len(vagas_coluna)):
+                print(f"{vagas+1} - {vagas_coluna[vagas]}")
+            vaga_alterar = int(input("Qual vaga deseja alterar?\n> "))
+
+            # Verificar se a skill e a vaga existem na matriz
+            if 1 <= skill_alterar <= len(skills_linha) and 1 <= vaga_alterar <= len(vagas_coluna):
+                skill_index = skill_alterar - 1
+                vaga_index = vaga_alterar - 1
+            else:
+                print("Skill ou vaga inválida.")
+
+            if skill_index == -1 or vaga_index == -1:
+                print("ERRO: Skill ou vaga não encontrada na matriz")
+                continue
+
             while True:
-                for vagas in range(len(vagas_linha)):
-                    print(f"{vagas+1} - {vagas_linha[vagas]}")
-                vaga_alterar = int(input("Qual vaga deseja alterar?\n> "))
-                
-                for skills in range(len(skills_coluna)):
-                    print(f"{skills+1} - {skills_coluna[skills]}")
-                skill_alterar = int(input("Qual skill deseja alterar?\n> "))
-                
-                # Verificar se a vaga e a skill existem na matriz
-                if 1 <= vaga_alterar <= len(vagas_linha) and 1 <= skill_alterar <= len(skills_coluna):
-                    vaga_index = vaga_alterar - 1
-                    skill_index = skill_alterar - 1
-                else:
-                    print("Vaga ou skill inválida.")
-
-                if vaga_index == -1 or skill_index == -1:
-                    print("ERRO: Vaga ou skill não encontrada na matriz")
-                    continue
-
-                while True:
-                    resposta = (input(f"A vaga '{vaga_alterar}' utiliza a skill '{skill_alterar}'? (S/N): "))
-                    if resposta.upper() == "S":
-                        matriz_vagas[vaga_index][skill_index] = True
-                        break
-                    elif resposta.upper() == "N":
-                        matriz_vagas[vaga_index][skill_index] = False
-                        break
-                    else:
-                        print("RESPOSTA INVALIDA, Digite S ou N")
-
-                if input("Deseja alterar outra vaga/skill? (S/N)\n> ").upper() == "S":
-                    continue
-                else:
+                resposta = (input(f"A skill '{skill_alterar}' é utilizada na vaga '{vaga_alterar}'? (S/N): "))
+                if resposta.upper() == "S":
+                    matriz_vagas[skill_index][vaga_index] = True
                     break
+                elif resposta.upper() == "N":
+                    matriz_vagas[skill_index][vaga_index] = False
+                    break
+                else:
+                    print("RESPOSTA INVALIDA, Digite S ou N")
+
+            if input("Deseja alterar outra skill/vaga? (S/N)\n> ").upper() == "S":
+                continue
+            else:
+                break
 
         case "5":
         # Printar a matriz
             print("                    ", end=' ')
-            for skill in skills_coluna:
-                print("{:<20}".format(skill), end=' ')
+            for vaga in vagas_coluna:
+                print("{:<20}".format(vaga), end=' ')
             print()
 
-            for i in range(len(matriz_vagas)):
-                print("{:<20}".format(vagas_linha[i]), end=' ')
-                for elemento in matriz_vagas[i]:
-                    print("{:<20}".format(str(elemento)), end=' ')
-                print()
+            if len(matriz_vagas) > 0:
+                for i in range(len(matriz_vagas)):
+                    print("{:<20}".format(skills_linha[i]), end=' ')
+                    for elemento in matriz_vagas[i]:
+                        print("{:<20}".format(str(elemento)), end=' ')
+                    print()
+            else:
+                for i in range(len(skills_linha)):
+                    print(f"{skills_linha[i]}")
 
         case "0":
             if resposta == "0":
