4a624562c4dd50d2e464786907437509289709ea
Luiz Hélio <luizhelionerd@hotmail.com>
2024-06-07 17:12:47 -0300

    Update Cadastro.py
    correção dos cases 2 e 3 para não dar erro quando a resposta for diferente de "S", Printar erro de remoção de skill e vaga qnd a planilha estiver com dados

--- Project/Cadastro.py
+++ Project/Cadastro.py
@@ -3,8 +3,8 @@ import os
 # Criação da matriz
 matriz_vagas = []
 
-skills_linha = ['JAVA', 'PYTHON', 'SQL', 'FIGMA', 'ADOBE XD', 'POWER BI']
-vagas_coluna = ['DEV. JAVA', 'ANALISTA DE DADOS', 'UX DESIGNER', 'DEV. FRONTEND', 'DESIGNER GRÁFICO']
+skills_linha = ['JAVA', 'PYTHON', 'JAVASCRIPT', 'POWER BI', 'FIGMA']
+vagas_coluna = ['DEV. JAVA', 'ANALISTA DE DADOS', 'DEV. FRONTEND', 'UX DESIGNER']
 
 while True:
     print(" ")
@@ -41,7 +41,7 @@ while True:
                             break
                         else:
                             print("ERRO: RESPOSTA INVALIDA, Digite S ou N")
-                matriz_vagas.append(skills)
+                matriz_vagas.append(skills)  
 
         case "2":
         # Cadastrar uma nova skill
@@ -50,12 +50,12 @@ while True:
                 conferir = input(f"{nova_Skill} está correto? (S/N)\n>")
                 if conferir.upper() == "S":
                     skills_linha.append(nova_Skill)
-
                     if input("Deseja Adicionar outra skill? (S/N)\n> ").upper() == "S":
                         continue
                     else:
                         break
                 elif conferir.upper() == "N":
+                    print("OPERAÇÃO CANCELADA.")
                     continue
                 else:
                     print("ERRO: RESPOSTA INVALIDA")
@@ -73,14 +73,31 @@ while True:
                         continue
                     else:
                         break
+                elif conferir.upper() == "N":
+                    print("OPERAÇÃO CANCELADA.")
+                    continue
+                else:
+                    print("ERRO: RESPOSTA INVALIDA")
+                    continue
 
         case "4":
         # Remover SKILLS
+            if len(matriz_vagas) != 0:
+                print("ERRO: A planilha está completa")
+                resetar = input("Deseja resetar a planilha para remover skill? (S/N)\n> ")
+                if resetar.upper() == "S":
+                    matriz_vagas = []
+                elif resetar.upper() == "N":
+                    continue
+                else:
+                    print("ERRO: RESPOSTA INVALIDA")
+                    continue
+
             for skills in range(len(skills_linha)):  # Imprime todas as skills
                 print(f"{skills_linha[skills]}")
             
             remove = input("\nQual skill deseja remover?\n> ").upper()
-            if remove in skills_linha:  # Confere se a vaga digitada existe no vetor skills_linha
+            if remove in skills_linha:  # Confere se a skill digitada existe no vetor skills_linha
                 conferir = input(f"Deseja mesmo remover {remove}? (S/N)\n> ")
                 if conferir.upper() == "S":
                     skills_linha.remove(remove)
@@ -91,9 +108,21 @@ while True:
             else:
                 print("ERRO: Skill não encontrada.")
 
+
         case "5":
         # Remover VAGAS
-            for vagas in range(len(vagas_coluna)):  # Imprime todas as skills
+            if len(matriz_vagas) != 0:
+                print("ERRO: A planilha está completa")
+                resetar = input("Deseja resetar a planilha para remover vaga? (S/N)\n> ")
+                if resetar.upper() == "S":
+                    matriz_vagas = []
+                elif resetar.upper() == "N":
+                    continue
+                else:
+                    print("ERRO: RESPOSTA INVALIDA")
+                    continue
+
+            for vagas in range(len(vagas_coluna)):  # Imprime todas as vagas
                 print(f"{vagas_coluna[vagas]}")
             
             remove = input("\nQual skill deseja remover?\n> ").upper()
@@ -111,8 +140,7 @@ while True:
         case "6":
         #Verificar se existem dados na matriz
             if len(matriz_vagas) == 0:
-                print("NENHUM DADO ENCONTRADO NA PLANILHA.")
-                continue
+                print("NENHUM DADO ENCONTRADO NA PLANILHA.\n")
 
         # Alterar dados na matriz
             for skills in range(len(skills_linha)):
